a4f5bc441f6d76e35449fa342fd030e736b846f3
Usa De Kit <israelmaou@gmail.com>
2024-01-23 15:51:53 +0000

    Initialisation de microservice caisse

--- core/src/main/java/com/defitech/pharmacie/core/repository/ProduitRepository.java
+++ core/src/main/java/com/defitech/pharmacie/core/repository/ProduitRepository.java
@@ -2,6 +2,8 @@ package com.defitech.pharmacie.core.repository;
 
 import com.defitech.pharmacie.core.entity.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProduitRepository extends JpaRepository<Produit, Integer> {
 }
